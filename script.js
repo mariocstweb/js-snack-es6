257fab510991ab2fb8c9a9bcd1a2d00dac91a049
mariocstweb <mariocstmra@gmail.com>
2024-01-16 13:36:16 +0100

    added guests array

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("ok");
+const tableName = "Tavolo Vip";
+
+const guests = [
+  "Brad Pitt",
+  "Johnny Depp",
+  "Lady Gaga",
+  "Cristiano Ronaldo",
+  "Georgina Rodriguez",
+  "Chiara Ferragni",
+  "Fedez",
+  "George Clooney",
+  "Amal Clooney",
+  "Maneskin",
+];
