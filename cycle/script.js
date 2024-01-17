f76ffc7dd50a4f93b8db53fca1152094aa71587f
mariocstweb <mariocstmra@gmail.com>
2024-01-17 14:30:32 +0100

    created a new object array

---
+++ cycle/script.js
@@ -0,0 +1,18 @@
+const bikes = [
+  {
+    brand: "Legnano",
+    weight: 10,
+  },
+  {
+    brand: "Bianchi",
+    weight: 12,
+  },
+  {
+    brand: "Piaggio",
+    weight: 11,
+  },
+  {
+    brand: "Lombardo",
+    weight: 9,
+  },
+];
