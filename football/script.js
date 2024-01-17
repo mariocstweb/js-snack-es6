ba2d4fcdc8ebf251a4bc0dd9f8d4fa21a6e18091
mariocstweb <mariocstmra@gmail.com>
2024-01-17 16:50:17 +0100

    created a new array

---
+++ football/script.js
@@ -0,0 +1,19 @@
+const footballSquads = [
+  {
+    name: "Inter",
+    score: 0,
+    foul: 0,
+  },
+  {
+    name: "Milan",
+    score: 0,
+    foul: 0,
+  },
+  {
+    name: "Roma",
+    score: 0,
+    foul: 0,
+  },
+];
+
+console.log(footballSquads);
