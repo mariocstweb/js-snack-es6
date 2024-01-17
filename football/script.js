6c4f6c17529730af434fe6f3882e6529a508cdb1
mariocstweb <mariocstmra@gmail.com>
2024-01-17 16:59:31 +0100

    added function for random number

--- football/script.js
+++ football/script.js
@@ -17,3 +17,18 @@ const footballSquads = [
 ];
 
 console.log(footballSquads);
+
+// Funzione per genrare un numero casuale
+const randomNumber = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomNum = randomNumber(1, 20);
+
+console.log(randomNum);
+
+// Tramite ciclo for riassegno i valori delle due chiavi
+footballSquads.forEach((squad) => {
+  squad.score = randomNumber(0, 70);
+  squad.foul = randomNumber(0, 20);
+});
+
+console.log(footballSquads);
