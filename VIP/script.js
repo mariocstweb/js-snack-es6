17baf05df181517bba5672169dd5d58f8a8de222
mariocstweb <mariocstmra@gmail.com>
2024-01-17 09:39:58 +0100

    fixed script js vip

--- VIP/script.js
+++ VIP/script.js
@@ -17,27 +17,33 @@ const guests = [
 ];
 
 // Inizializzo un nuovo vuoto array vuoto dove inserirò i nuovi oggetti generati
-guestsList = [];
+// guestsList = [];
 
-for (let i = 0; i < guests.length; i++) {
-  const newObject = {
-    table: tableName,
-    guess: guests[i],
-    place: i + 1,
-  };
-  guestsList.push(newObject);
-}
+// for (let i = 0; i < guests.length; i++) {
+//   const newObject = {
+//     table: tableName,
+//     guess: guests[i],
+//     place: i + 1,
+//   };
+//   guestsList.push(newObject);
+// }
 
 // console.log(guestsList);
 
-// Flagvuoto
+// Metodo map
+const guestsList = guests.map((guest, i) => {
+  return { table: tableName, name: guest, place: i + 1 };
+});
+console.log(guestsList);
+
+// Flag vuoto
 let tableList = "";
 
 for (let i = 0; i < guestsList.length; i++) {
   const guest = guestsList[i];
   tableList += `
   <li>
-    Tavolo: ${guest.table}, Ospite: ${guest.guess}, Posto: ${guest.place}
+    Tavolo: ${guest.table}, Ospite: ${guest.name}, Posto: ${guest.place}
   </li>
 `;
 }
