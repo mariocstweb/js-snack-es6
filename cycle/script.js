904e1095958d7800d9f073f3fb784af27b3fd3dd
mariocstweb <mariocstmra@gmail.com>
2024-01-17 16:42:22 +0100

    added reduce method

--- cycle/script.js
+++ cycle/script.js
@@ -1,3 +1,4 @@
+// Creo un array di oggetti di bici con il loro peso
 const bikes = [
   {
     brand: "Legnano",
@@ -16,3 +17,11 @@ const bikes = [
     weight: 9,
   },
 ];
+
+// Utilizzo il metodo reduce per trovare la bicicletta con il peso minore
+const minWeightBike = bikes.reduce(
+  (min, bike) => (bike.weight < min.weight ? bike : min),
+  bikes[0]
+);
+
+console.log("Bicicletta con peso minore:", minWeightBike);
